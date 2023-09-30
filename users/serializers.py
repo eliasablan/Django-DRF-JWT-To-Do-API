51ef9ea1cd339b7a2acc84fa128a49361f3bbc75
Elias Ablan <eliasablan93@gmail.com>
2023-09-30 01:04:42 -0400

    avoid duplicated emails and superuser automation logic

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from decouple import config as dc_config
+
+SUPERUSER_EMAIL = dc_config('SUPERUSER_EMAIL')
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'password', 'email', 'is_superuser', 'is_staff')
 
-    def create(self, validated_data):
-        user = super(UserSerializer, self).create(validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+    def validate(self, data):
+        # Comprueba si el correo electrónico ya está en uso.
+        existing_user = User.objects.filter(email=data['email']).first()
+        if existing_user:
+            # Si el correo electrónico ya está en uso, lanza un error.
+            raise serializers.ValidationError({'email': ['El correo electrónico ya está en uso.']})
+
+        # Comprueba si el correo electrónico coincide con el SUPERUSER_EMAIL.
+        if data['email'] == SUPERUSER_EMAIL:
+            data['is_superuser'] = True
+            data['is_staff'] = True
+
+        return data
